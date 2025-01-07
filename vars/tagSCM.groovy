ddf36e5d69aeebe98870e3e3cfd2e605ed82f84f
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-08 00:21:38 +0700

    Update tagSCM.groovy

--- vars/tagSCM.groovy
+++ vars/tagSCM.groovy
@@ -1,12 +1,11 @@
 def call(Map config) {
-    stage('Tag and Push to GitHub') {
-        withCredentials([usernamePassword(credentialsId: config.credentialsId, usernameVariable: 'user', passwordVariable: 'pass')]) {
+         withCredentials([usernamePassword(credentialsId: config.credentialsId, usernameVariable: 'user', passwordVariable: 'pass')]) {
             sh """
             git config --global user.email "duy.nguyentadinh@gmail.com"
             git config --global user.name "duydinhhh"
             git tag -a ${config.deployTag} -m "Deployed ${config.PIPELINE_TYPE} environment"
             git push https://$user:$pass@github.com/${config.repo}.git ${config.deployTag}
             """
-        }
+        
     }
 }
