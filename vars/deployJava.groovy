cb274f385e0e50e7bbbc77c3a290fd4dd5a010a6
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-08 00:20:45 +0700

    Update deployJava.groovy

--- vars/deployJava.groovy
+++ vars/deployJava.groovy
@@ -1,7 +1,3 @@
 def call(Map config){
-  stage('Deploy java') {
-    
-        sh "java -jar ${config.NEXUS_ARTIFACT_ID}-${config.ARTIFACT_VERS}-${config.DEPLOY_TAG}.jar &"
-    
-  }
+    sh "java -jar ${config.NEXUS_ARTIFACT_ID}-${config.ARTIFACT_VERS}-${config.DEPLOY_TAG}.jar &"
 }
