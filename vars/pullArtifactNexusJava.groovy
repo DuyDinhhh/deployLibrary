89f034b4018d2425ea9a1498e512c18d610d5f0c
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-07 16:11:35 +0700

    Create pullArtifactNexusJava.groovy

---
+++ vars/pullArtifactNexusJava.groovy
@@ -0,0 +1,10 @@
+def call(Map config) {
+    stage('Pull Artifact') {
+        withCredentials([usernamePassword(credentialsId: config['for-nexus'], passwordVariable: 'pass', usernameVariable: 'user')]) {
+            sh """
+            curl -u $user:$pass -O \
+            http://${config.NEXUS_URL}/repository/${config.NEXUS_REPOSITORY}/${config.NEXUS_GROUP}/${config.NEXUS_ARTIFACT_ID}/${config.ARTIFACT_VERS}-${config.DEPLOY_TAG}/${config.NEXUS_ARTIFACT_ID}-${config.ARTIFACT_VERS}-${config.DEPLOY_TAG}.jar
+            """
+        }
+    }
+}
