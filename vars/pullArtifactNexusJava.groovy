76bdf18fe31e47f3d6a083bc57efecebad3ffee3
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-14 22:51:05 +0700

    Update pullArtifactNexusJava.groovy

--- vars/pullArtifactNexusJava.groovy
+++ vars/pullArtifactNexusJava.groovy
@@ -1,8 +1,10 @@
 def call() {
-     withCredentials([usernamePassword(credentialsId: env.NEXUS_CREDENTIALS_ID, passwordVariable: 'pass', usernameVariable: 'user')]) {
-        sh """
-        curl -u $user:$pass -O \
-        http://${env.NEXUS_URL}/repository/${env.NEXUS_REPOSITORY}/${env.NEXUS_GROUP}/${env.NEXUS_ARTIFACT_ID}/${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}/${env.NEXUS_ARTIFACT_ID}-${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}.jar
-        """
+     stage('Pull Artifact from Nexus') {
+          withCredentials([usernamePassword(credentialsId: env.NEXUS_CREDENTIALS_ID, passwordVariable: 'pass', usernameVariable: 'user')]) {
+             sh """
+             curl -u $user:$pass -O \
+             http://${env.NEXUS_URL}/repository/${env.NEXUS_REPOSITORY}/${env.NEXUS_GROUP}/${env.NEXUS_ARTIFACT_ID}/${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}/${env.NEXUS_ARTIFACT_ID}-${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}.jar
+             """
+          }
      }
 }
