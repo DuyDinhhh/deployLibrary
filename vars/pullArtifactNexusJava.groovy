b0455bc19634b24cb50a078622ae0c9af254e057
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-14 15:55:29 +0700

    Update pullArtifactNexusJava.groovy

--- vars/pullArtifactNexusJava.groovy
+++ vars/pullArtifactNexusJava.groovy
@@ -1,5 +1,5 @@
 def call() {
-     withCredentials([usernamePassword(credentialsId: config['for-nexus'], passwordVariable: 'pass', usernameVariable: 'user')]) {
+     withCredentials([usernamePassword(credentialsId: env.NEXUS_CREDENTIALS_ID, passwordVariable: 'pass', usernameVariable: 'user')]) {
         sh """
         curl -u $user:$pass -O \
         http://${env.NEXUS_URL}/repository/${env.NEXUS_REPOSITORY}/${env.NEXUS_GROUP}/${env.NEXUS_ARTIFACT_ID}/${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}/${env.NEXUS_ARTIFACT_ID}-${env.ARTIFACT_VERS}-${env.DEPLOY_TAG}.jar
