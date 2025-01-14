f6c558ff16fff86c6aec671233d2e9c60595ca09
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-14 11:34:20 +0700

    Update healthCheck.groovy

--- vars/healthCheck.groovy
+++ vars/healthCheck.groovy
@@ -1,8 +1,9 @@
 def call() {
+    stage('Health Check'){        
         script {
             try {
                 sleep(20)   
-                def response = httpRequest url: 'http://192.168.64.35:8080'
+                def response = httpRequest url: 'http://192.168.66.5:8080'
                 if (response.status != 200) {
                     error("Health Check Failed with HTTP Status: ${response.status}")
                 }
@@ -11,5 +12,5 @@ def call() {
                 error("Health Check Failed")
             }
         }
-    
+    }
 }
